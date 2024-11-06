45988645038eb4d889e720ac5c753cd1111a13f1
jiyoon <rlawldbs9128@gmail.com>
2024-11-06 19:43:20 +0900

    ScheduleRequestDto update

--- src/main/java/com/example/schedule/Lv1/dto/ScheduleRequstDto.java
+++ src/main/java/com/example/schedule/Lv1/dto/ScheduleRequstDto.java
@@ -1,4 +1,15 @@
 package com.example.schedule.Lv1.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+
 public class ScheduleRequstDto {
+    //요청할 데이터
+    private String name;
+    private String password;
+    private String title;
+    private String contents;
 }
--- src/main/java/com/example/schedule/Lv1/entity/schedule.java
+++ src/main/java/com/example/schedule/Lv1/entity/schedule.java
@@ -6,15 +6,18 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Getter
-@AllArgsConstructor
+@Getter //데이터 읽어오기
+@AllArgsConstructor //생성자
 
 public class schedule {
+    //schedule 에서 필요한 데이터
     private long id;
     private String name;
     private String password;
     private String title;
     private String contents;
+    //작성일, 수정일 저장하기 위해 LocalDateTime 사용
+    //LocalDateTime : 날짜와 시간을 모두 포함, LocalDate+LocalTime
     private LocalDateTime createDate;
     private LocalDateTime updateDate;
 }

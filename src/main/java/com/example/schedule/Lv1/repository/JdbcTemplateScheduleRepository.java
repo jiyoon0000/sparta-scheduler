851708e86d640333e2e653393e38ecbdfaebc770
jiyoon <rlawldbs9128@gmail.com>
2024-11-07 01:55:56 +0900

    repository update...ing

--- src/main/java/com/example/schedule/Lv1/repository/JdbcTemplateScheduleRepository.java
+++ src/main/java/com/example/schedule/Lv1/repository/JdbcTemplateScheduleRepository.java
@@ -1,4 +1,22 @@
 package com.example.schedule.Lv1.repository;
 
-public class JdbcTemplateScheduleRepository {
+import com.example.schedule.Lv1.dto.ScheduleRequstDto;
+import com.example.schedule.Lv1.dto.ScheduleResponseDto;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+
+@Repository
+public class JdbcTemplateScheduleRepository implements ScheduleRepository{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplateScheduleRepository(DataSource dataSource){
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public ScheduleResponseDto saveSchedule(ScheduleRequstDto scheduleRequest){
+
+    }
 }

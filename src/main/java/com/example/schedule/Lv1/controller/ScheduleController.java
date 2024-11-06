32ec078eb7a0362560e5d8b58ab6086c61880705
jiyoon <rlawldbs9128@gmail.com>
2024-11-07 01:55:36 +0900

    ScheduleController update

--- src/main/java/com/example/schedule/Lv1/controller/ScheduleController.java
+++ src/main/java/com/example/schedule/Lv1/controller/ScheduleController.java
@@ -1,4 +1,26 @@
 package com.example.schedule.Lv1.controller;
 
+import com.example.schedule.Lv1.dto.ScheduleRequstDto;
+import com.example.schedule.Lv1.dto.ScheduleResponseDto;
+import com.example.schedule.Lv1.repository.JdbcTemplateScheduleRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/schedules")
 public class ScheduleController {
+
+    private final JdbcTemplateScheduleRepository scheduleRepository;
+
+    public ScheduleController(JdbcTemplateScheduleRepository scheduleRepository){
+        this.scheduleRepository = scheduleRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<ScheduleResponseDto> createSchedule(@RequestBody ScheduleRequstDto requstDto){
+        ScheduleResponseDto responseDto = scheduleRepository.saveSchedule(requstDto);
+        return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+    }
+
 }

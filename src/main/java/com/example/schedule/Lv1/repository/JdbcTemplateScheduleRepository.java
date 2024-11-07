1d360661b47b6b35da481480b75e3d7ee9ad520d
jiyoon <rlawldbs9128@gmail.com>
2024-11-07 11:28:34 +0900

    update

--- src/main/java/com/example/schedule/Lv1/repository/JdbcTemplateScheduleRepository.java
+++ src/main/java/com/example/schedule/Lv1/repository/JdbcTemplateScheduleRepository.java
@@ -1,22 +1,57 @@
 package com.example.schedule.Lv1.repository;
 
-import com.example.schedule.Lv1.dto.ScheduleRequstDto;
+import com.example.schedule.Lv1.dto.ScheduleReqeustDto;
 import com.example.schedule.Lv1.dto.ScheduleResponseDto;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @Repository
 public class JdbcTemplateScheduleRepository implements ScheduleRepository{
 
     private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert simpleJdbcInsert;
 
     public JdbcTemplateScheduleRepository(DataSource dataSource){
         this.jdbcTemplate = new JdbcTemplate(dataSource);
+        this.simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+                simpleJdbcInsert.withTableName("schedules").usingGeneratedKeyColumns("id");
     }
 
     @Override
-    public ScheduleResponseDto saveSchedule(ScheduleRequstDto scheduleRequest){
+    public ScheduleResponseDto saveSchedule(ScheduleReqeustDto scheduleRequest){
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("name", scheduleRequest.getName());
+        //parameters.put("password", scheduleRequest.getPassword()); 이거도 주어진 조건대로 주석처리
+        parameters.put("title", scheduleRequest.getTitle());
+        parameters.put("contents", scheduleRequest.getContents());
+        parameters.put("createDate", LocalDateTime.now());
+        parameters.put("updateDate", LocalDateTime.now());
 
+        Number key = simpleJdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
+
+        return findScheduleById(key.longValue()).orElse(null);
+    }
+
+    @Override
+    public Optional<ScheduleResponseDto> findScheduleById(Long id){
+        return jdbcTemplate.query(
+                "SELECT id, name, title, contents, createDate, updateDate FROM schedules WHERE id = ?",
+                (rs, rowNum) -> new ScheduleResponseDto(
+                            rs.getLong("id"),
+                            rs.getString("name"),
+                            rs.getString("title"),
+                            rs.getString("contents"),
+                            rs.getTimestamp("createDate").toLocalDateTime(),
+                            rs.getTimestamp("updateDate").toLocalDateTime()
+                    ), id
+        ).stream().findAny();
     }
 }

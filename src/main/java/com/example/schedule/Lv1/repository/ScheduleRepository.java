ecbdbaaad12ad581ca0cb732601fb7fa58367820
jiyoon <rlawldbs9128@gmail.com>
2024-11-07 11:28:18 +0900

    ScheduleRepository
    class -> interface

--- src/main/java/com/example/schedule/Lv1/repository/ScheduleRepository.java
+++ src/main/java/com/example/schedule/Lv1/repository/ScheduleRepository.java
@@ -1,11 +1,11 @@
 package com.example.schedule.Lv1.repository;
 
-import com.example.schedule.Lv1.dto.ScheduleRequstDto;
+import com.example.schedule.Lv1.dto.ScheduleReqeustDto;
 import com.example.schedule.Lv1.dto.ScheduleResponseDto;
 
 import java.util.Optional;
 
-public class ScheduleRepository {
-    ScheduleResponseDto saveSchedule(ScheduleRequstDto scheduleRequest);
+public interface ScheduleRepository {
+    ScheduleResponseDto saveSchedule(ScheduleReqeustDto scheduleRequest);
     Optional<ScheduleResponseDto> findScheduleById(Long id);
 }
